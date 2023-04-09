4fd7ded2468a3680a16d1678fbab6e679a2b129a
caglarop <caglarop@gmail.com>
2023-04-09 02:55:11 +0200

    .

--- src/components/layouts/MainFooter.tsx
+++ src/components/layouts/MainFooter.tsx
@@ -14,8 +14,8 @@ export default function MainFooter() {
         <div className="flex w-full items-center justify-center gap-4">
           <div>
             Made with ❤️ by{' '}
-            <a href="https://devlify.io" target="_devlify_io">
-              Devlify.io
+            <a href="https://devlified.com" target="_devlify_io">
+              Devlified.com
             </a>
           </div>
           {version && version.length > 0 && (
