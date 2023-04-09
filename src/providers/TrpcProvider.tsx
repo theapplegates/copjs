1c6f87e7b706cc550615b96b0b8eba41795ee9f0
caglarop <caglarop@gmail.com>
2023-04-09 05:25:59 +0200

    .

--- src/providers/TrpcProvider.tsx
+++ src/providers/TrpcProvider.tsx
@@ -20,7 +20,7 @@ export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({
 
   const url = process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc/`
-    : 'http://localhost:3000/api/trpc/';
+    : `http://${process.env.SITE_URL}/api/trpc/`;
 
   const [trpcClient] = useState(() =>
     trpc.createClient({
